035cf2e3a8bc562cc49b6b34583da63898f480d1
Muhammad Taha <60589832+smuhammadtaha@users.noreply.github.com>
2021-11-26 03:03:23 +0500

    all done

--- src/App.js
+++ src/App.js
@@ -17,58 +17,6 @@ function App() {
   const [marks , setMark] = useState(0);
   const Result = `Your Marks is ${marks}`;
   const none = '';
-  // const [data, setdata]  = useState ([
-  //   {
-  //     question : "Q1: Which of the following is used to create Web Pages ?",
-  //     answers : ["C++","Java","HTML","JVM"],
-  //     correctAns : "HTML",
-  //   },
-  //   {
-  //     question : "Q2: HTML is a set of markup",
-  //     answers : ["tags","sets","attributes","None of these"],
-  //     correctAns : "tags",
-  //   },
-  //   {
-  //     question : "Q3: HTML tags are used to describe document",
-  //     answers : ["defination","content","language","model"],
-  //     correctAns : "content",
-  //   },
-  //   {
-  //     question : "Q4: HTML program is saved using ______ extension",
-  //     answers : [".htmn",".html",".css",".js"],
-  //     correctAns : ".html",
-  //   },
-  //   {
-  //     question : "Q5: HTML program can be read and rendered by _______.",
-  //     answers : ["Compiler","Server","Web Browser","Interpreter"],
-  //     correctAns : "Web Browser",
-  //   },
-  //   {
-  //     question : "Q6: HTML tags are surrounded by _____ brackets.",
-  //     answers : ["Angle","Curly","Round","Square"],
-  //     correctAns : "Angle",
-  //   },
-  //   {
-  //     question : "Q7: HTML document can contain _______.",
-  //     answers : ["Tags","Plain Text","Attributes","All of above"],
-  //     correctAns : "All of above",
-  //   },
-  //   {
-  //     question : "Q8: Page designed in HTML is called as _________.",
-  //     answers : ["Server Page","Web Page","Media Page","none of above"],
-  //     correctAns : "Web Page",
-  //   },
-  //   {
-  //     question : "Q9: Which of the following is the first web browser ?",
-  //     answers : ["Nexus","Netscape Navigator","Internet Explorer","Mosaic"],
-  //     correctAns : "Nexus",
-  //   },
-  //   {
-  //     question : "Q10: Who created the first web browser ?",
-  //     answers : ["Tim Berners Lee","Jacobs","Marc Andeersen","Mozilla foundation"],
-  //     correctAns : "Tim Berners Lee",
-  //   }
-  // ]);
   const [dataInd, setDataInd] = useState(0);
   const [showResult, setShowResult] = useState(false);
   const nextQues = () =>{
